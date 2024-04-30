7e7a03b62ad984a2140294cf82d53550c29b42c8
Kwonyougsu <ysun5311@gmail.com>
2024-04-30 17:26:06 +0900

    HP변수 ,음수 오류 수정

--- Player.cs
+++ Player.cs
@@ -5,18 +5,18 @@
     public int Level { get; }
     public int Atk { get; }
     public int Def { get; }
-    public int Hp { get; }
+    public int MaxHp { get; }
     public int Gold { get; set; }
-    public int Attacked_Hp { get; set; }
-    public Player(string name, string job, int level, int atk, int def, int hp, int gold, int attacked_Hp)
+    public int Current_Hp { get; set; }
+    public Player(string name, string job, int level, int atk, int def, int maxhp, int gold, int current_hp)
     {
         Name = name;
         Job = job;
         Level = level;
         Atk = atk;
         Def = def;
-        Hp = hp;
+        MaxHp = maxhp;
         Gold = gold;
-        Attacked_Hp = attacked_Hp;
+        Current_Hp = current_hp;
     }
 }
