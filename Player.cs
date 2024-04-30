f4d3470825c259c0388fe012b9e3e007eb0d4142
Kwonyougsu <ysun5311@gmail.com>
2024-04-30 21:04:16 +0900

    exp 레벨업
    내일 레벨별 if문을 다시 점검할 예정

--- Player.cs
+++ Player.cs
@@ -6,7 +6,7 @@ internal class Player
     private int PlayerExp = 0;
     public string Name { get; set; }
     public string Job { get; }
-    public int Level { get; }
+    public int Level { get; set; }
     public double Atk { get; set; }
     public int Def { get; set; }
     public int MaxHp { get; set; }
@@ -32,11 +32,25 @@ internal class Player
     }
     public void PlayerLevelUp() 
     {
-        
-        Console.WriteLine(PlayerExp);
-        Console.WriteLine("[내 정 보]");
-        Console.WriteLine($"Lv.{Level} {Name} ({Job})");
-        Console.WriteLine($"HP :{MaxHp} -> {Current_Hp}");
+        //MaxExp를 만들어서 넣고 초기화? 하는 방향성으로 ㄱㄱ 
+        int AddExp = Exp + PlayerExp;
+        if (PlayerExp == 10 || PlayerExp == 35 || PlayerExp == 65 || PlayerExp == 100)
+        { 
+            Console.WriteLine("[내 정 보]");
+            Console.WriteLine($"Lv.{Level} {Name} ({Job}) -> Lv.{Level+1} {Name} ({Job})");
+            Console.WriteLine($"HP :{MaxHp} -> {Current_Hp}");
+            Console.WriteLine("레벨업!");
+            Level = Level + 1; 
+            Exp = 0; // exp를 누적
+        }
+        else
+        {
+            Console.WriteLine("[내 정 보]");
+            Console.WriteLine($"Lv.{Level} {Name} ({Job})");
+            Console.WriteLine($"HP :{MaxHp} -> {Current_Hp}");
+            Console.WriteLine($"Exp :{Exp} -> {AddExp}");
+            Exp = Exp+ PlayerExp;
+        }
         PlayerExp = 0;
     }
 
