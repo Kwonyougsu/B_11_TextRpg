5e5cd2e32d3bd5220dca9e8a24b07faacfa10f4e
Kwonyougsu <ysun5311@gmail.com>
2024-05-01 13:26:56 +0900

    아이템 등급 분류, 몬스터 드랍테이블 추가
    현재 0~3(낡은) 까지만 사용, 층수 구현 후 아이템 등급 시스템 도입 예정

--- Player.cs
+++ Player.cs
@@ -5,11 +5,12 @@ internal class Player
 {
     private int PlayerExp = 0;
     private int MaxExp = 0;
-    private int[] requireExp = {0,10, 35, 65, 100 };
+    private int[] requireExp = { 0, 10, 35, 65, 100 };
 
     Random random = new Random();
 
     public static List<Item> inventory;
+    public List<Item> monsterDropList = new List<Item>();
 
     public string Name { get; set; }
     public string Job { get; }
@@ -21,7 +22,7 @@ internal class Player
     public int Current_Hp { get; set; }
     public int Exp { get; set; }
     public int Postion { get; set; }
-    public Player(string name, string job, int level, double atk, int def, int maxhp, int gold, int current_hp, int exp,int postion)
+    public Player(string name, string job, int level, double atk, int def, int maxhp, int gold, int current_hp, int exp, int postion)
     {
         Name = name;
         Job = job;
@@ -34,20 +35,20 @@ internal class Player
         Exp = exp;
         Postion = postion;
     }
-    
+
     public void GetExp(int monsterexp)
     {
         PlayerExp += monsterexp;
     }
-    public void PlayerLevelUp() 
+    public void PlayerLevelUp()
     {
-        int LevelUpExp = requireExp[Level]; 
+        int LevelUpExp = requireExp[Level];
         //MaxExp를 만들어서 넣고 초기화? 하는 방향성으로 ㄱㄱ 
         MaxExp = Exp + PlayerExp;
-        if (MaxExp >= LevelUpExp) 
-        { 
+        if (MaxExp >= LevelUpExp)
+        {
             Console.WriteLine("[내 정 보]");
-            Console.WriteLine($"Lv.{Level} {Name} ({Job}) -> Lv.{Level+1} {Name} ({Job})");
+            Console.WriteLine($"Lv.{Level} {Name} ({Job}) -> Lv.{Level + 1} {Name} ({Job})");
             MaxHp = MaxHp + 20;
             Console.WriteLine("레벨업!");
             Console.WriteLine("체력이 회복되었습니다");
@@ -63,7 +64,7 @@ internal class Player
             Console.WriteLine($"Lv.{Level} {Name} ({Job})");
             Console.WriteLine($"HP :{MaxHp} -> {Current_Hp}");
             Console.WriteLine($"Exp :{Exp} -> {MaxExp}");
-            Exp = Exp+ PlayerExp;
+            Exp = Exp + PlayerExp;
         }
         PlayerExp = 0;
     }
@@ -72,7 +73,9 @@ internal class Player
         int gold = random.Next(100, 500);
         int postion = random.Next(0, 2);
         int itemdrop = random.Next(0, 10);
-        int count = 1;
+
+        List<Item> monsterDropList = RandomMonster.monsterdrop.GetRange(0, 3);
+
         Console.WriteLine("");
         Console.WriteLine("[획득 아이템]");
         ConsoleUtility.ShowTitle($"{gold} G");
@@ -85,11 +88,14 @@ internal class Player
         }
         if (itemdrop < 5)
         {
-            Console.Write("부식된 검");
-            inventory.Add(new Item("부식된 검", "세월이 흐름에 따라 부식된 검.", ItemType.WEAPON, 2, 0, 0, 1000));
-            ConsoleUtility.ShowTitle($"{count}");
+            if (monsterDropList.Count > 0)
+            {
+                int randomIndex = random.Next(0, monsterDropList.Count);
+                Item droppedItem = monsterDropList[randomIndex];
+                inventory.Add(droppedItem);
+                Console.Write($"{droppedItem.Name} - ");
+                ConsoleUtility.ShowTitle("1");
+            }
         }
-        
     }
-
 }
